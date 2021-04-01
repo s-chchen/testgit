d95ddea4513da566fb2b9b9b1c4b44dbf068cebc
s-chchen <1843093222@qq.com>
2021-04-01 12:43:15 +0800

    Signed-off-by: s-chchen <1843093222@qq.com>v1.2

--- chapter03/src/com/itheima/cglib/CglibTest.java
+++ chapter03/src/com/itheima/cglib/CglibTest.java
@@ -1,7 +1,8 @@
 package com.itheima.cglib;
 
 public class CglibTest {
-
+	
+	//v1.2
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		CglibProxy cglibProxy=new CglibProxy();
